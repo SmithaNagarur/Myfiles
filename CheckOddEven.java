f5810534b8e5e8d30c7cdb90252ee350e4676a1e
SmithaNagarur <nsmithareddy20@gmail.com>
2016-09-27 22:39:47 -0700

    Add files via upload

---
+++ CheckOddEven.java
@@ -0,0 +1,21 @@
+
+public class CheckOddEven {
+
+	public static void main(String[] args) {
+
+  //Write a program called CheckOddEven which prints "Odd Number" if the int variable
+//“number" is odd, or “Even Number” otherwise.
+		//The program shall always print “BYE!” before exiting.
+		
+		int number = 567543;
+		if ((number%2)==0){
+			System.out.println("Even");
+		}
+		else {
+			System.out.println("Odd");
+		}
+		System.out.println("BYE");
+		}
+	}
+
+
